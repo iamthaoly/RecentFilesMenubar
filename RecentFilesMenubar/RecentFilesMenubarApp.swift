3eba8938f2491e4ba62dda1264efca436b583c84
Ly <lyttt.dev@gmail.com>
2022-07-27 15:35:53 +0700

    Initial Commit

---
+++ RecentFilesMenubar/RecentFilesMenubarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecentFilesMenubarApp.swift
+//  RecentFilesMenubar
+//
+//  Created by ly on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecentFilesMenubarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
