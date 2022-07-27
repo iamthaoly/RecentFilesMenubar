aff8d2521abe03d89d70636fad3274491372d2ee
Ly <lyttt.dev@gmail.com>
2022-07-27 15:55:56 +0700

    Skeleton for the app

--- RecentFilesMenubar/ContentView.swift
+++ RecentFilesMenubar/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            FileItem()
+            FileItem()
         }
     }
 }
@@ -21,5 +19,40 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct FileItem: View {
+    var body: some View {
+    
+        VStack {
+            HStack() {
+                RoundedRectangle(cornerRadius: 5)
+                    .fill(Color.black)
+                    .frame(width: 100, height: 60)
+                VStack(alignment: .leading) {
+                    Text("How to use this file.pdf")
+                        .font(.headline)
+                    HStack() {
+                        Image(systemName: "square.and.arrow.down")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Text("Desktop")
+                            .font(.body)
+                    }
+                    Text("1.6 MB")
+                }
+                .frame(maxWidth: .infinity)
+                Text("7:25")
+                    .frame(maxHeight: .infinity, alignment: .top)
+            }
+            .padding()
+            .frame(width: 300.0, height: 150)
+            Button("Show in Finder") {
+                
+            }
+        }
     }
 }
